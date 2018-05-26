344f584a6d6c5427314a7d6e0cf49335bfd57be8
Rui Marinho <me@ruimarinho.net>
2018-05-26 01:42:45 +0100

    [Controls] Add VSMTestPage

---
+++ Xamarin.Forms.Controls/VSMTestPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Xamarin.Forms.Controls
+{
+	public partial class VSMTestPage : ContentPage
+	{
+		public VSMTestPage()
+		{
+			InitializeComponent();
+		}
+
+		void Handle_Clicked(object sender, System.EventArgs e)
+		{
+			entry.IsEnabled = false;
+		}
+
+		protected override void OnAppearing()
+		{
+			base.OnAppearing();
+
+			Device.StartTimer(TimeSpan.FromSeconds(2), () =>
+			{
+				var storyboard = this.Resources["myStoryboard"] as Storyboard;
+				storyboard.Begin();
+				return false;
+			});
+		}
+	}
+}
