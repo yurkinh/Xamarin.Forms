534ff2a7e59427779d281ec18f2d7277a80a46a2
Rui Marinho <me@ruimarinho.net>
2019-02-01 19:31:27 +0000

    [Controls] Fix vm for CarouselXamlGallery

--- Xamarin.Forms.Controls/GalleryPages/CollectionViewGalleries/CarouselViewGalleries/CarouselXamlGallery.xaml.cs
+++ Xamarin.Forms.Controls/GalleryPages/CollectionViewGalleries/CarouselViewGalleries/CarouselXamlGallery.xaml.cs
@@ -14,29 +14,31 @@ namespace Xamarin.Forms.Controls.GalleryPages.CollectionViewGalleries.CarouselVi
 		}
 	}
 
-
 	internal class CarouselViewModel : ViewModelBase2
 	{
+		int _count;
+		int _position;
+		ObservableCollection<CarouselItem> _items;
+
 		public CarouselViewModel(int intialItems = 5)
 		{
 			var items = new List<CarouselItem>();
 			for (int i = 0; i < intialItems; i++)
 			{
-				items.Add(new CarouselItem());
+				items.Add(new CarouselItem("cardBackground"));
 			}
 
+			MessagingCenter.Subscribe<ExampleTemplateCarousel>(this, "remove", (obj) => Items.Remove(obj.BindingContext as CarouselItem));
+
 			Items = new ObservableCollection<CarouselItem>(items);
-			//Items.CollectionChanged += ItemsCollectionChanged;
-			Count = Items.Count;
+			Items.CollectionChanged += ItemsCollectionChanged;
+			Count = Items.Count - 1;
 		}
 
-		void Items_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+		void ItemsCollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
 		{
+			Count = Items.Count - 1;
 		}
-
-
-		int _count;
-		ObservableCollection<CarouselItem> _items;
 
 		public int Count
 		{
@@ -44,16 +46,26 @@ namespace Xamarin.Forms.Controls.GalleryPages.CollectionViewGalleries.CarouselVi
 			set { SetProperty(ref _count, value); }
 		}
 
+		public int Position
+		{
+			get { return _position; }
+			set { SetProperty(ref _position, value); }
+		}
+
 		public ObservableCollection<CarouselItem> Items
 		{
 			get { return _items; }
 			set { SetProperty(ref _items, value); }
 		}
-
 	}
 
 	internal class CarouselItem
 	{
+		public CarouselItem(string image)
+		{
+			Image = image;
+		}
 
+		public string Image { get; set; }
 	}
 }
