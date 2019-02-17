bc7b1bcfcd44691f78a99299fbc4ae19a35ceb59
David Ortinau <david.ortinau@microsoft.com>
2019-02-16 20:26:12 -0600

    removed ineffective uitest

--- Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/StatusBarHeight.cs
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/StatusBarHeight.cs
@@ -2,17 +2,8 @@
 using Xamarin.Forms.CustomAttributes;
 using Xamarin.Forms.Internals;
 
-#if UITEST
-using Xamarin.Forms.Core.UITests;
-using Xamarin.UITest;
-using NUnit.Framework;
-#endif
-
 namespace Xamarin.Forms.Controls.Issues
 {
-#if UITEST
-	[Category(UITestCategories.ManualReview)]
-#endif
 	[Preserve(AllMembers = true)]
 	[Issue(IssueTracker.None, 20090125, "Get height of the status bar", PlatformAffected.iOS | PlatformAffected.Android)]
 	public class StatusBarHeightTest : TestContentPage // or TestMasterDetailPage, etc ...
@@ -25,21 +16,10 @@ namespace Xamarin.Forms.Controls.Issues
 				AutomationId = "StatusBarHeight",
 				Text = $"The status bar is {Device.Info.StatusBarHeight} units tall. "
 					+ Environment.NewLine + "iOS should be 44."
-					+ Environment.NewLine + "Android should be 24.,
+					+ Environment.NewLine + "Android should be 24.",
 				HorizontalOptions = LayoutOptions.Center,
 				VerticalOptions = LayoutOptions.Center
 			};
 		}
-
-#if UITEST
-		[Test]
-		public void StatusBarHeightTest() 
-		{
-			// Delete this and all other UITEST sections if there is no way to automate the test. Otherwise, be sure to rename the test and update the Category attribute on the class. Note that you can add multiple categories.
-			RunningApp.Screenshot ("I am at Issue 1");
-			RunningApp.WaitForElement (q => q.Marked ("IssuePageLabel"));
-			RunningApp.Screenshot ("I see the Label");
-		}
-#endif
 	}
 }
