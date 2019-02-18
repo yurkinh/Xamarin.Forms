90115f9237c00c3a27977b80b58c77377c2456c2
Matthew Leibowitz <mattleibow@live.com>
2019-02-18 03:23:31 +0200

    Added a XAML page for testing

---
+++ Xamarin.Forms.Sandbox/CodePage.cs
@@ -0,0 +1,57 @@
+﻿namespace Xamarin.Forms.Sandbox
+{
+	public class CodePage : ContentPage
+	{
+		Label _label;
+
+		public CodePage()
+		{
+			Title = "Code Page";
+			Content = new StackLayout
+			{
+				Padding = 20,
+				Spacing = 20,
+				Children =
+				{
+					(_label = new Label { Text = "Click a button..." }),
+					new Button
+					{
+						Text = "Default",
+
+						Command = new Command(() => _label.Text = "Clicked the default button."),
+					},
+					new Button
+					{
+						Text = "Material",
+
+						Command = new Command(() => _label.Text = "Clicked the Material button."),
+
+						Visual = VisualMarker.Material,
+					},
+					new Button
+					{
+						Text = "SkiaSharp",
+						CornerRadius = 4,
+						BackgroundColor = Color.DarkOrange,
+						BorderColor = Color.Red,
+						BorderWidth = 4,
+
+						Command = new Command(() => _label.Text = "Clicked the SkiaSharp button."),
+
+						Visual = SkiaSharpVisual.SkiaSharp.Instance,
+
+						// TODO: implement the sizing logic for the custom renderers
+						WidthRequest = 100,
+						HeightRequest = 100
+					},
+
+					new Button
+					{
+						Text = "Go To XAML",
+						Command = new Command(() => Navigation.PushAsync(new XamlPage()))
+					}
+				}
+			};
+		}
+	}
+}
---
+++ Xamarin.Forms.Sandbox/XamlPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Xamarin.Forms.Sandbox
+{
+	public partial class XamlPage : ContentPage
+	{
+		public XamlPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
