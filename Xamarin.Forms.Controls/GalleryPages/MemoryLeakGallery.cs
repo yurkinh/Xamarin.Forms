30db82fced7a125ef2ee92223936ba616d6b9a87
Pavel Yakovlev <v-payako@microsoft.com>
2019-04-03 18:35:32 +0300

    fixes getting content

--- Xamarin.Forms.Controls/GalleryPages/MemoryLeakGallery.cs
+++ Xamarin.Forms.Controls/GalleryPages/MemoryLeakGallery.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Threading.Tasks;
-using Xamarin.Forms.Controls.GalleryPages.CollectionViewGalleries;
 using Xamarin.Forms.Internals;
 
 namespace Xamarin.Forms.Controls
@@ -15,28 +13,23 @@ namespace Xamarin.Forms.Controls
 
 		Button checkResult;
 
-		[Preserve(AllMembers = true)]
-		class LeakPage : ContentPage
-		{
-			public LeakPage(View content)
-			{
-				Content = content;
-			}
-		}
-
-		Button MakeButton(string text, View content)
+		Button MakeButton(string text, Func<View> getContent)
 		{
 			return new Button
 			{
 				Text = text,
 				Command = new Command(async () =>
 				{
-					await Detail.Navigation.PushAsync(new LeakPage(content));
-					var pageToRemove = Detail.Navigation.NavigationStack[1];
-					wref.Add(new WeakReference(pageToRemove));
-					await Task.Delay(200);
+					View content = getContent();
+					await Detail.Navigation.PushAsync(new ContentPage { Content = content });
+					Detail.Navigation.RemovePage(Detail.Navigation.NavigationStack[0]);
+
+					await Detail.Navigation.PushAsync(new ContentPage());
+					var pageToRemove = Detail.Navigation.NavigationStack[0];
 					Detail.Navigation.RemovePage(pageToRemove);
 
+					wref.Add(new WeakReference(pageToRemove));
+					wref.Add(new WeakReference(content));
 					result.Text = string.Empty;
 				})
 			};
@@ -47,7 +40,7 @@ namespace Xamarin.Forms.Controls
 			result = new Label
 			{
 				FontSize = 16,
-				Text = ""
+				Text = string.Empty
 			};
 
 			checkResult = new Button
@@ -61,71 +54,56 @@ namespace Xamarin.Forms.Controls
 					GC.WaitForPendingFinalizers();
 					GC.Collect();
 
-					if (wref.Count > 1)
+					if (wref.Count < 4)
+						return;
+
+					if (wref[0].IsAlive || wref[1].IsAlive)
 					{
-						if (wref[wref.Count - 2].IsAlive)
-						{
-							result.Text = "Failed";
-							result.TextColor = Color.Red;
-						}
-						else
-						{
-							result.Text = "Success";
-							result.TextColor = Color.DarkGreen;
-						}
+						Title = result.Text = "Failed";
+						result.TextColor = Color.Red;
 					}
+					else
+					{
+						Title = result.Text = "Success";
+						result.TextColor = Color.DarkGreen;
+					}
+					wref.Clear();
 				})
 			};
 
-			if ((Device.Flags?.IndexOf(ExperimentalFlags.CollectionViewExperimental) ?? -1) == -1)
+			Master = new ContentPage
 			{
-				List<string> flags;
-				if (Device.Flags != null)
-					flags = new List<string>(Device.Flags);
-				 else
-					flags = new List<string>();
-				flags.Add(ExperimentalFlags.CollectionViewExperimental);
-				Device.SetFlags(flags.ToArray());
-			}
-
-			Master = new ContentPage {
 				Title = "menu",
-				Content = new StackLayout
+				Content = new ScrollView
 				{
-					Children =
+					Content = new StackLayout
 					{
-						new Label { Text = "Click on button twice, then check result" },
-						result,
-						MakeButton("Empty page", null),
-						MakeButton("Entry", new Entry { Text = "Entry" }),
-						MakeButton("Editor", new Editor { Text = "Editor" }),
-						MakeButton("Image", new Image { BackgroundColor = Color.Azure, HeightRequest = 50 }),
-						MakeButton("Button", new Button { Text = "Button" }),
-						MakeButton("BoxView", new BoxView { Color = Color.Azure }),
-						MakeButton("CarouselView Horizontal", new CarouselView
+						Children =
 						{
-							ItemsLayout = new ListItemsLayout(ItemsLayoutOrientation.Horizontal)
-							{
-								SnapPointsType = SnapPointsType.MandatorySingle,
-								SnapPointsAlignment = SnapPointsAlignment.Center
-							},
-							ItemTemplate = ExampleTemplates.CarouselTemplate(),
-						}),
-						MakeButton("CarouselView Vertical", new CarouselView
-						{
-							ItemsLayout = new ListItemsLayout(ItemsLayoutOrientation.Vertical)
-							{
-								SnapPointsType = SnapPointsType.MandatorySingle,
-								SnapPointsAlignment = SnapPointsAlignment.Center
-							},
-							ItemTemplate = ExampleTemplates.CarouselTemplate(),
-						}),
-						MakeButton("ActivityIndicator", new ActivityIndicator
-						{
-							Color = Color.Azure,
-							BackgroundColor = Color.Black
-						}),
-						checkResult
+							checkResult,
+							new Label { Text = "Click on button twice, then check result" },
+							result,
+							MakeButton("Empty page", () => null),
+							MakeButton(nameof(ActivityIndicator), () => new ActivityIndicator { IsRunning = true }),
+							MakeButton(nameof(BoxView), () => new BoxView { Color = Color.Azure }),
+							MakeButton(nameof(Button), () => new Button { Text = "Button" }),
+							MakeButton(nameof(DatePicker), () => new DatePicker ()),
+							MakeButton(nameof(Label), () => new Label { Text = "Label" }),
+							MakeButton(nameof(Entry), () => new Entry { Text = "Entry" }),
+							MakeButton(nameof(Editor), () => new Editor { Text = "Editor" }),
+							MakeButton(nameof(Image), () => new Image { BackgroundColor = Color.Azure, HeightRequest = 50 }),
+							MakeButton(nameof(ImageButton), () => new ImageButton { BackgroundColor = Color.Azure, HeightRequest = 50 }),
+							MakeButton(nameof(WebView), () => new WebView { BackgroundColor = Color.Azure, HeightRequest = 50 }),
+							MakeButton(nameof(ProgressBar), () => new ProgressBar { BackgroundColor = Color.Azure, Progress = 0.5 }),
+							MakeButton(nameof(Picker), () => new Picker { BackgroundColor = Color.Azure, HeightRequest = 50 }),
+							MakeButton(nameof(OpenGLView), () => new OpenGLView ()),
+							MakeButton(nameof(SearchBar), () => new SearchBar ()),
+							MakeButton(nameof(Slider), () => new Slider ()),
+							MakeButton(nameof(Stepper), () => new Stepper ()),
+							MakeButton(nameof(Switch), () => new Switch ()),
+							MakeButton(nameof(TimePicker), () => new TimePicker ()),
+							MakeButton(nameof(TableView), () => new TableView ()),
+						}
 					}
 				}
 			};
