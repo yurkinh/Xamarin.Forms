73b2f40bc3708447feb87183983425157177e922
Rui Marinho <me@ruimarinho.net>
2019-06-12 20:01:58 +0100

    [Controls] Add repo for issue #6458

---
+++ Xamarin.Forms.Controls.Issues/Xamarin.Forms.Controls.Issues.Shared/Issue6458.cs
@@ -0,0 +1,38 @@
+﻿using Xamarin.Forms.CustomAttributes;
+using Xamarin.Forms.Internals;
+
+#if UITEST
+using Xamarin.Forms.Core.UITests;
+using Xamarin.UITest;
+using NUnit.Framework;
+#endif
+
+namespace Xamarin.Forms.Controls.Issues
+{
+#if UITEST
+	[Category(UITestCategories.ManualReview)]
+#endif
+	[Preserve(AllMembers = true)]
+	[Issue(IssueTracker.Github, 6458, "[Android] Fix load TitleIcon on non app compact", PlatformAffected.Android)]
+	public class Issue6458 : TestContentPage // or TestMasterDetailPage, etc ...
+	{
+		protected override void Init()
+		{
+			NavigationPage.SetTitleIconImageSource(this, new FileImageSource { File = "bank.png" });
+			Content = new Label
+			{
+				AutomationId = "IssuePageLabel",
+				Text = "Make sure you run this on Non AppCompact Activity"
+			};
+		}
+
+#if UITEST && __ANDROID__
+		[Test]
+		public void Issue6458Test() 
+		{
+			RunningApp.WaitForElement (q => q.Marked ("IssuePageLabel"));
+			RunningApp.Screenshot ("You should see the bank icon on the ActionBar");
+		}
+#endif
+	}
+}
