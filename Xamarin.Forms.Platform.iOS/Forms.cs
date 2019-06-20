70034bf90ec7d82573b2aee1e18f2dcb2bcf5fef
E.Z. Hart <hartez@gmail.com>
2019-06-20 16:39:28 -0600

    Turns out we need the EmptyCollectionViewReloadWorkaround for older iOS versions

--- Xamarin.Forms.Platform.iOS/Forms.cs
+++ Xamarin.Forms.Platform.iOS/Forms.cs
@@ -36,6 +36,7 @@ namespace Xamarin.Forms
 		static bool? s_isiOS9OrNewer;
 		static bool? s_isiOS10OrNewer;
 		static bool? s_isiOS11OrNewer;
+		static bool? s_isiOS12OrNewer;
 #endif
 
 #if __MOBILE__
@@ -67,6 +68,16 @@ namespace Xamarin.Forms
 				if (!s_isiOS11OrNewer.HasValue)
 					s_isiOS11OrNewer = UIDevice.CurrentDevice.CheckSystemVersion(11, 0);
 				return s_isiOS11OrNewer.Value;
+			}
+		}
+
+		internal static bool IsiOS12OrNewer
+		{
+			get
+			{
+				if (!s_isiOS12OrNewer.HasValue)
+					s_isiOS12OrNewer = UIDevice.CurrentDevice.CheckSystemVersion(12, 0);
+				return s_isiOS12OrNewer.Value;
 			}
 		}
 #endif
