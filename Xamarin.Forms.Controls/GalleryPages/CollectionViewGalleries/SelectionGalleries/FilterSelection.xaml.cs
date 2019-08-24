ed187732bc701f34b4638ed4eef639d14eb25134
E.Z. Hart <hartez@gmail.com>
2019-08-23 19:03:29 -0600

    Remove unnecessary stuff from test page

--- Xamarin.Forms.Controls/GalleryPages/CollectionViewGalleries/SelectionGalleries/FilterSelection.xaml.cs
+++ Xamarin.Forms.Controls/GalleryPages/CollectionViewGalleries/SelectionGalleries/FilterSelection.xaml.cs
@@ -1,10 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace Xamarin.Forms.Controls.GalleryPages.CollectionViewGalleries.SelectionGalleries
@@ -19,7 +13,6 @@ namespace Xamarin.Forms.Controls.GalleryPages.CollectionViewGalleries.SelectionG
 			InitializeComponent();
 
 			CollectionView.ItemsSource = _demoFilteredItemSource.Items;
-			CollectionView.SelectionChanged += CollectionViewSelectionChanged;
 
 			SearchBar.SearchCommand = new Command(() =>
 			{
@@ -29,31 +22,10 @@ namespace Xamarin.Forms.Controls.GalleryPages.CollectionViewGalleries.SelectionG
 			ResetButton.Clicked += ResetButtonClicked;
 		}
 
-		private void CollectionViewSelectionChanged(object sender, SelectionChangedEventArgs e)
-		{
-			UpdateSelectionInfo();
-		}
-
 		void ResetButtonClicked(object sender, EventArgs e)
 		{
 			_demoFilteredItemSource = new DemoFilteredItemSource(new Random().Next(3, 50));
 			CollectionView.ItemsSource = _demoFilteredItemSource.Items;
-		}
-
-		void UpdateSelectionInfo()
-		{
-			var current = "[none]";
-
-			if (CollectionView.SelectionMode == SelectionMode.Multiple)
-			{
-				current = CollectionView?.SelectedItems.ToCommaSeparatedList();
-			}
-			else if (CollectionView.SelectionMode == SelectionMode.Single)
-			{
-				current = ((CollectionViewGalleryTestItem)CollectionView?.SelectedItem)?.Caption;
-			}
-
-			CurrentSelection.Text = $"Selection: {current}";
 		}
 	}
 }
